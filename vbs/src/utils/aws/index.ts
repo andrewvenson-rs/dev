d40977ac46cc4bb772891d9218273f8bd6c385f2
Andrew Venson <andrew.venson@redshelf.com>
2024-10-03 14:03:38 -0500

    wrap it

--- vbs/src/utils/aws/index.ts
+++ vbs/src/utils/aws/index.ts
@@ -3,20 +3,21 @@ import {
   GetSecretValueCommand,
 } from "@aws-sdk/client-secrets-manager";
 
-const client = new SecretsManagerClient({
-  region: "us-east-1",
-});
 
 export const getSecret = async (
   secretName: string,
 ): Promise<string | undefined> => {
   try {
+    const client = new SecretsManagerClient({
+      region: "us-east-1",
+    });
     const command = new GetSecretValueCommand({
       SecretId: `${process.env.environmentName}/${secretName}`,
     });
+    if (!command) return undefined;
     const response = await client.send(command);
     if (!response) return undefined
-    return response.SecretString;
+    return response?.SecretString || undefined;
   } catch (error) {
     console.error("Error retrieving secret:", error);
   }
