e6eb28f9ebc3d90bcd962678c277918eca4bc22f
Andrew Venson <andrew.venson@redshelf.com>
2024-10-02 22:12:16 -0500

    getting back json parsed responses

---
+++ vbs/src/utils/aws/index.ts
@@ -0,0 +1,16 @@
+import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
+
+const client = new SecretsManagerClient({
+	region: 'us-east-1'
+});
+
+export const getSecret = async (secretName: string): Promise<string | undefined> => {
+	try {
+		const command = new GetSecretValueCommand({ SecretId: secretName });
+		const response = await client.send(command);
+		return response.SecretString;
+	} catch (error) {
+		console.error('Error retrieving secret:', error);
+	}
+	return undefined;
+};
